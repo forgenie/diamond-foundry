cf27e52c236c250cb2b23e853b6b5f4c2495f954
Alexe Spataru <alexeluca.spataru@gmail.com>
2023-05-03 05:50:53 +0300

    feat: add delegate call

---
+++ src/utils/DelegateCall.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+error DelegateCall_noDelegateCall_DelegateNotAllowed();
+error DelegateCall_onlyDelegateCall_OnlyDelegate();
+
+abstract contract DelegateCall {
+    address private immutable _this;
+
+    constructor() {
+        _this = address(this);
+    }
+
+    modifier onlyDelegateCall() {
+        if (address(this) == _this) revert DelegateCall_onlyDelegateCall_OnlyDelegate();
+        _;
+    }
+
+    modifier noDelegateCall() {
+        if (address(this) != _this) revert DelegateCall_noDelegateCall_DelegateNotAllowed();
+        _;
+    }
+}
