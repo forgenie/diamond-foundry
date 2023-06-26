e5666c4847d3f0540ee717d6dddda6b41b41d8a6
Alexe Spataru <alexeluca.spataru@gmail.com>
2023-06-26 15:21:03 +0300

    fix: lint

--- test/facets/ownable/behavior/transferOwnership.t.sol
+++ test/facets/ownable/behavior/transferOwnership.t.sol
@@ -3,11 +3,7 @@ pragma solidity 0.8.19;
 
 import { OwnableFacetTest } from "../ownable.t.sol";
 
-import {
-    OwnableBehavior,
-    Ownable_NotOwner,
-    Ownable_ZeroAddress
-} from "src/facets/ownable/OwnableBehavior.sol";
+import { OwnableBehavior, Ownable_NotOwner, Ownable_ZeroAddress } from "src/facets/ownable/OwnableBehavior.sol";
 
 contract Ownable_transferOwnership is OwnableFacetTest {
     function test_RevertsWhen_CallerIsNotOwner() public {
