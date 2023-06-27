3e5027dcfd85b5eae99b6605f6e53bd798fea448
Alexe Spataru <alexeluca.spataru@gmail.com>
2023-06-27 19:19:24 +0300

    test: nft owned facet

--- src/facets/nft-owned/NFTOwnedFacet.sol
+++ src/facets/nft-owned/NFTOwnedFacet.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT License
 pragma solidity 0.8.19;
 
+import { Facet } from "src/facets/Facet.sol";
 import { INFTOwned } from "./INFTOwned.sol";
 import { NFTOwnedBase } from "./NFTOwnedBase.sol";
 
-contract NFTOwnedFacet is INFTOwned, NFTOwnedBase {
+contract NFTOwnedFacet is INFTOwned, NFTOwnedBase, Facet {
+    function initialize(address nftContract, uint256 tokenId) external onlyInitializing {
+        __NFTOwned_init(nftContract, tokenId);
+    }
+
     /// @inheritdoc INFTOwned
     function token() external view override returns (address nftContract, uint256 tokenId) {
         return _token();
