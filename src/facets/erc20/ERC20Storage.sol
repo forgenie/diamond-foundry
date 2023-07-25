aa1890cd4ecc54b7f47d6eaab360a5d9d9e5e17d
Alexe Spataru <alexeluca.spataru@gmail.com>
2023-07-25 16:29:00 +0300

    feat: add erc20 implementation

--- src/facets/erc20/ERC20Storage.sol
+++ src/facets/erc20/ERC20Storage.sol
@@ -2,18 +2,19 @@
 pragma solidity 0.8.19;
 
 library ERC20Storage {
-    bytes32 internal constant ERC20_STORAGE_SLOT = keccak256("diamond.standard.erc20.storage");
+    bytes32 private constant _ERC20_STORAGE_SLOT = keccak256("erc20.storage");
 
     struct Layout {
-        uint256 totalSupply;
         mapping(address => uint256) balances;
         mapping(address => mapping(address => uint256)) allowances;
+        uint256 totalSupply;
+        uint8 decimals;
         string name;
         string symbol;
     }
 
     function layout() internal pure returns (Layout storage l) {
-        bytes32 position = ERC20_STORAGE_SLOT;
+        bytes32 position = _ERC20_STORAGE_SLOT;
         assembly {
             l.slot := position
         }
--- src/facets/erc20/IERC20.sol
+++ src/facets/erc20/IERC20.sol
@@ -1,4 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+/**
+ * @title ERC20 interface
+ * @dev See https://eips.ethereum.org/EIPS/eip-20.
+ */
+interface IERC20 {
+    /**
+     * @notice Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @notice Returns the amount of tokens owned by an account.
+     * @param account The account to get the balance of.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @notice Moves an amount of tokens from the caller's account to another account.
+     * @param to The account where the tokens are transferred to.
+     * @param amount The number of tokens transferred.
+     * @return Boolean value indicating whether the operation succeeded.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @notice Returns the remaining number of tokens that spender will be allowed to spend on behalf of owner.
+     * @dev This value changes when {approve} or {transferFrom} are called and is zero by default.
+     * @param owner The account that owns the tokens.
+     * @param spender The account that is allowed to transfer the tokens.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @notice Sets an amount as the allowance of spender over the caller's tokens.
+     * @param spender The account that is allowed to transfer the tokens.
+     * @param amount The number of tokens that are approved.
+     * @return bool value indicating whether the operation succeeded.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @notice Transferrs an amount of tokens from an account to other using the allowance mechanism.
+     * @dev Amount is deducted from the caller's allowance.
+     * @param from The account where the tokens are transferred from.
+     * @param to The account where the tokens are transferred to.
+     * @param amount The number of tokens transferred.
+     * @return bool value indicating whether the operation succeeded.
+     */
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
