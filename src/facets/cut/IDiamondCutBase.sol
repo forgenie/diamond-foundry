295a6e9dcce7cc19e07fb34f3fb636a2c406b69c
RohanNero <rohanzarathustra@gmail.com>
2024-08-20 18:05:46 -0400

    add documentation

--- src/facets/cut/IDiamondCutBase.sol
+++ src/facets/cut/IDiamondCutBase.sol
@@ -4,17 +4,37 @@ pragma solidity >=0.8.20;
 import { IDiamond } from "src/IDiamond.sol";
 
 interface IDiamondCutBase {
-    //todo: add docs
+    /// @notice Thrown when the facet cut has no selectors. 
     error DiamondCut_SelectorArrayEmpty(address facet);
+
+    /// @notice Thrown when the facet address is zero address.
     error DiamondCut_FacetIsZeroAddress();
+
+    /// @notice Thrown when the facet address isn't a contract.
     error DiamondCut_FacetIsNotContract(address facet);
+
+    /// @notice Thrown when the facet cut action doesn't exist.
     error DiamondCut_IncorrectFacetCutAction();
+
+    /// @notice Thrown when a facet cut selector is zero.
     error DiamondCut_SelectorIsZero();
+
+    /// @notice Thrown when a facet cut selector being added already exists.
     error DiamondCut_FunctionAlreadyExists(bytes4 selector);
+
+    /// @notice Thrown when facet cut tries to remove selectors from another facet.
     error DiamondCut_CannotRemoveFromOtherFacet(address facet, bytes4 selector);
+
+    /// @notice Thrown when diamond cut tries to replace a facet with itself.
     error DiamondCut_FunctionFromSameFacet(bytes4 selector);
+
+    /// @notice Thrown when a diamond cut tries to replace a facet that doesn't exist.
     error DiamondCut_NonExistingFunction(bytes4 selector);
+
+     /// @notice Thrown when the diamond cut tries to remove an immutable facet.
     error DiamondCut_ImmutableFacet();
+
+    /// @notice Thrown when trying to send init data to an address that isn't a contract.
     error DiamondCut_InitIsNotContract(address init);
 
     /**
